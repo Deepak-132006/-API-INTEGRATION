5b375b9dcd0e11673cd53c2e2460003c314fa268
Deepak-132006 <deepakn132006@gmail.com>
2025-05-12 23:08:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Weather from './Components/Weather.jsx'
+import './Components/Weather.css'
+
+function App() {
+  return (
+    <Weather/>
+  )
+}
+
+export default App
